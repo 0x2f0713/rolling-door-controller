5b5e8e3541ff5e8ef7d5597545aed8d8f75a26f6
0x2f0713 <namhaiha0308@gmail.com>
2020-05-24 22:27:20 +0700

    add config resolver

---
+++ src/app/services/config/config-resolver.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { take, map } from 'rxjs/operators';
+
+import { ConfigService } from './config.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigResolverService implements Resolve<Observable<any>>{
+
+  constructor(private cf: ConfigService) { }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.cf.getConfig().pipe(
+      take(1),
+      map(config => config)
+    )
+  }
+}
